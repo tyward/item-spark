fd7c3a8c68e9ed77c11826f0a3cca415b3f38513
tyler <tyler@192.168.51.170>
2017-05-11 22:13:06 -0400

    Adding bridge to run ITEM from within spark

--- src/main/java/edu/columbia/tjw/item/spark/ItemClassifier.java
+++ src/main/java/edu/columbia/tjw/item/spark/ItemClassifier.java
@@ -27,6 +27,10 @@ import edu.columbia.tjw.item.data.ItemStatusGrid;
 import edu.columbia.tjw.item.fit.ItemFitter;
 import edu.columbia.tjw.item.optimize.ConvergenceException;
 import edu.columbia.tjw.item.util.random.RandomTool;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import org.apache.spark.ml.classification.ProbabilisticClassifier;
 import org.apache.spark.ml.linalg.Vector;
 import org.apache.spark.ml.param.ParamMap;
@@ -39,26 +43,45 @@ import org.apache.spark.sql.Dataset;
  * @param <R>
  * @param <T>
  */
-public class ItemClassifier<S extends ItemStatus<S>, R extends ItemRegressor<R>, T extends ItemCurveType<T>> extends ProbabilisticClassifier<Vector, ItemClassifier<S, R, T>, ItemClassificationModel<S, R, T>> {
+public class ItemClassifier<S extends ItemStatus<S>, R extends ItemRegressor<R>, T extends ItemCurveType<T>>
+        extends ProbabilisticClassifier<Vector, ItemClassifier<S, R, T>, ItemClassificationModel<S, R, T>>
+        implements Cloneable
+{
 
     private static final long serialVersionUID = 0x7cc313e747f68becL;
 
-    private final ItemClassifierSettings<S, R, T> _settings;
-    private final String _uid;
+    private ItemClassifierSettings<S, R, T> _settings;
+    private String _uid;
 
-    public ItemClassifier(final ItemClassifierSettings<S, R, T> settings_) {
+    public ItemClassifier(final ItemClassifierSettings<S, R, T> settings_)
+    {
+        if (null == settings_)
+        {
+            throw new NullPointerException("Settings cannot be null.");
+        }
+
         _settings = settings_;
-        _uid = RandomTool.randomString(64);
+    }
+
+    private void updateSettings(ItemClassifierSettings<S, R, T> settings_)
+    {
+        if (null == settings_)
+        {
+            throw new NullPointerException("Settings cannot be null.");
+        }
+
+        _settings = settings_;
     }
 
     @Override
-    public ItemClassifier<S, R, T> copy(ParamMap paramMap_) {
-        final ItemClassifierSettings<S, R, T> settings = paramMap_.apply(ItemSettingsParam.singleton());
-        return new ItemClassifier<>(settings);
+    public ItemClassifier<S, R, T> copy(ParamMap paramMap_)
+    {
+        return this.defaultCopy(paramMap_);
     }
 
     @Override
-    public ItemClassificationModel<S, R, T> train(final Dataset<?> data_) {
+    public ItemClassificationModel<S, R, T> train(final Dataset<?> data_)
+    {
         //This is pretty filthy, but it will get the job done. Though, only locally. 
         //final String featureCol = this.getFeaturesCol();
         //final String labelCol = this.getLabelCol();
@@ -73,12 +96,15 @@ public class ItemClassifier<S extends ItemStatus<S>, R extends ItemRegressor<R>,
 
         final int maxParams = _settings.getMaxParamCount();
 
-        try {
-            for (int i = 0; i < 3; i++) {
+        try
+        {
+            for (int i = 0; i < 3; i++)
+            {
                 final int usedParams = fitter.getBestParameters().getEffectiveParamCount();
                 final int remainingParams = maxParams - usedParams;
 
-                if (remainingParams < 1) {
+                if (remainingParams < 1)
+                {
                     //Not enough params to do anything, break out.
                     break;
                 }
@@ -91,10 +117,13 @@ public class ItemClassifier<S extends ItemStatus<S>, R extends ItemRegressor<R>,
 
                 final int curveAvailable = maxParams - fitter.getBestParameters().getEffectiveParamCount();
 
-                if (curveAvailable > 3) {
+                if (curveAvailable > 3)
+                {
                     //Now expand the model by adding curves.
                     fitter.expandModel(_settings.getCurveRegressors(), null, curveAvailable);
-                } else {
+                }
+                else
+                {
                     break;
                 }
 
@@ -111,7 +140,9 @@ public class ItemClassifier<S extends ItemStatus<S>, R extends ItemRegressor<R>,
                 fitter.runAnnealingByEntry(_settings.getCurveRegressors(), false);
             }
 
-        } catch (final ConvergenceException e) {
+        }
+        catch (final ConvergenceException e)
+        {
             throw new RuntimeException(e);
         }
 
@@ -123,7 +154,44 @@ public class ItemClassifier<S extends ItemStatus<S>, R extends ItemRegressor<R>,
     }
 
     @Override
-    public String uid() {
+    public ItemClassifier<S, R, T> clone()
+    {
+        try
+        {
+            //Force this to be a deep clone by serializing and deserializing the object. 
+            final ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+            final ObjectOutputStream oOut = new ObjectOutputStream(bOut);
+            oOut.writeObject(this);
+
+            final ByteArrayInputStream bIn = new ByteArrayInputStream(bOut.toByteArray());
+            final ObjectInputStream oIn = new ObjectInputStream(bIn);
+
+            final ItemClassifier<S, R, T> output = (ItemClassifier<S, R, T>) oIn.readObject();
+            return output;
+        }
+        catch (final Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+
+//        try {
+//        final ItemClassifier<S, R, T> output = (ItemClassifier<S, R, T>) super.clone();
+//        return output;
+//        }
+//        catch(final CloneNotSupportedException e) {
+//            
+//        }
+    }
+
+    @Override
+    public synchronized String uid()
+    {
+        //Hideous hack because this is being called in the superclass constructor.
+        if (null == _uid)
+        {
+            _uid = RandomTool.randomString(64);
+        }
+
         return _uid;
     }
 
