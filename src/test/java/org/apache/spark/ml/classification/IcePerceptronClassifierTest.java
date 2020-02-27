5dd72fdf3aa5c2137c2dc903582bfac7c219642e
tyler <tyler@192.168.51.170>
2020-02-27 13:19:28 -0500

    Adding initial skeleton of the ICE classifier.

--- src/test/java/org/apache/spark/ml/classification/IcePerceptronClassifierTest.java
+++ src/test/java/org/apache/spark/ml/classification/IcePerceptronClassifierTest.java
@@ -22,7 +22,63 @@ class IcePerceptronClassifierTest
 
         SparkContext context = spark.sparkContext();
 
+        spark.udf().register("toArrayLambda", (x) -> (((DenseVector) x).toArray()),
+                DataTypes.createArrayType(DataTypes.DoubleType));
 
+        Dataset<Row> frame = generateData(spark);
+
+
+//        String[] inputCols = new String[]{"AGE", "MTM_LTV", "INCENTIVE", "CREDIT_SCORE", "FIRSTTIME_BUYER", "TERM",
+//                "MI_PERCENT", "UNIT_COUNT", "ORIG_CLTV", "ORIG_DTI", "ORIG_UPB", "ORIG_INTRATE",
+//                "PREPAYMENT_PENALTY"};
+
+        String[] inputCols = new String[]{"MTM_LTV", "INCENTIVE", "FIRSTTIME_BUYER",
+                "UNIT_COUNT", "ORIG_CLTV", "ORIG_DTI", "ORIG_INTRATE", "PREPAYMENT_PENALTY"};
+
+        VectorAssembler assembler =
+                new VectorAssembler().setInputCols(inputCols).setOutputCol("features").setHandleInvalid("skip");
+        Dataset<Row> transformed = assembler.transform(frame);
+        Dataset<Row>[] datasets = transformed.randomSplit(new double[]{0.25, 0.75}, 12345);
+
+        Dataset<Row> fitting = datasets[0].limit(10 * 1000);
+        Dataset<Row> testing = datasets[1];
+
+        final ClassificationModel mlpModel;
+        final ClassificationModel iceModel;
+
+        {
+            MultilayerPerceptronClassifier mlp_fitter = new MultilayerPerceptronClassifier().setLabelCol("NEXT_STATUS");
+            mlp_fitter.setLayers(new int[]{inputCols.length, 5, 3}).setSeed(1234L).setLabelCol("NEXT_STATUS")
+                    .setMaxIter(10).setSolver("l-bfgs");
+            mlpModel = mlp_fitter.fit(fitting);
+        }
+
+        {
+            IcePerceptronClassifier iceFitter = new IcePerceptronClassifier().setLabelCol("NEXT_STATUS");
+            iceFitter.setLayers(new int[]{inputCols.length, 5, 3}).setSeed(1234L).setLabelCol("NEXT_STATUS")
+                    .setMaxIter(10).setSolver("l-bfgs");
+            iceModel = iceFitter.fit(fitting);
+        }
+
+        Dataset<Row> fitEvalMlp = evaluate(spark, fitting, mlpModel);
+        Dataset<Row> testEvalMlp = evaluate(spark, testing, mlpModel);
+
+        Dataset<Row> fitEvalIce = evaluate(spark, fitting, iceModel);
+        Dataset<Row> testEvalIce = evaluate(spark, testing, iceModel);
+
+        System.out.println("Fitting eval MLP.");
+        fitEvalMlp.show();
+        System.out.println("Testing eval MLP.");
+        testEvalMlp.show();
+
+        System.out.println("Fitting eval ICE.");
+        fitEvalIce.show();
+        System.out.println("Testing eval ICE.");
+        testEvalIce.show();
+    }
+
+    private Dataset<Row> generateData(final SparkSession spark)
+    {
         Dataset<Row> frame = spark.read().parquet("/Users/tyler/sync-workspace/nyu_class/data/df_c_1");
 
         frame = frame.filter("MTM_LTV > 0.0").filter("MTM_LTV < 2.0");
@@ -43,47 +99,11 @@ class IcePerceptronClassifierTest
         frame = frame.withColumn("actual_p", functions.expr(" case when next_status = 0 then 1.0 else 0.0 end"));
         frame = frame.withColumn("actual_c", functions.expr(" case when next_status = 1 then 1.0 else 0.0 end"));
         frame = frame.withColumn("actual_3", functions.expr(" case when next_status = 2 then 1.0 else 0.0 end"));
-
-
-//        String[] inputCols = new String[]{"AGE", "MTM_LTV", "INCENTIVE", "CREDIT_SCORE", "FIRSTTIME_BUYER", "TERM",
-//                "MI_PERCENT", "UNIT_COUNT", "ORIG_CLTV", "ORIG_DTI", "ORIG_UPB", "ORIG_INTRATE",
-//                "PREPAYMENT_PENALTY"};
-
-        String[] inputCols = new String[]{"MTM_LTV", "INCENTIVE", "FIRSTTIME_BUYER",
-                "UNIT_COUNT", "ORIG_CLTV", "ORIG_DTI", "ORIG_INTRATE", "PREPAYMENT_PENALTY"};
-
-        frame.show();
-
-        VectorAssembler assembler =
-                new VectorAssembler().setInputCols(inputCols).setOutputCol("features").setHandleInvalid("skip");
-        Dataset<Row> transformed = assembler.transform(frame);
-
-        MultilayerPerceptronClassifier mlp_fitter = new MultilayerPerceptronClassifier().setLabelCol("NEXT_STATUS");
-        mlp_fitter.setLayers(new int[]{inputCols.length, 5, 3}).setSeed(1234L).setLabelCol("NEXT_STATUS")
-                .setMaxIter(10).setSolver("l-bfgs");
-
-        Dataset<Row>[] datasets = transformed.randomSplit(new double[]{0.25, 0.75}, 12345);
-
-        Dataset<Row> fitting = datasets[0].limit(10 * 1000);
-        Dataset<Row> testing = datasets[1];
-
-        MultilayerPerceptronClassificationModel model = mlp_fitter.fit(fitting);
-
-        spark.udf().register("toArrayLambda", (x) -> (((DenseVector) x).toArray()),
-                DataTypes.createArrayType(DataTypes.DoubleType));
-
-        Dataset<Row> fitEval = evaluate(spark, fitting, model);
-        Dataset<Row> testEval = evaluate(spark, testing, model);
-
-        System.out.println("Fitting eval.");
-        fitEval.show();
-        System.out.println("Testing eval.");
-        testEval.show();
-
+        return frame;
     }
 
     private Dataset<Row> evaluate(final SparkSession spark, Dataset<Row> data,
-                                  MultilayerPerceptronClassificationModel model)
+                                  ClassificationModel model)
     {
         Dataset<Row> testingResults = model.transform(data);
         testingResults = testingResults.withColumn("prob_array", functions.expr("toArrayLambda(probability)"));
@@ -98,10 +118,14 @@ class IcePerceptronClassifierTest
 
         testingResults.show();
 
-        testingResults.select(functions.expr("count(*)"), functions.expr("sum(crossEntropy)/count(*)"), functions.expr(
-                "sum(distEntropy)/count(*)")).show();
+        Dataset<Row> result = testingResults.select(functions.expr("count(*)"), functions.expr("sum" +
+                        "(crossEntropy)/count(*)"),
+                functions.expr(
+                        "sum(distEntropy)/count(*)"));
 
-        return testingResults;
+        result.show();
+
+        return result;
     }
 
 
